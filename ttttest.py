40310ec978dccc98ce2484f52f4dac0dd018c19f
StuLiu <1183862787@qq.com>
2019-05-06 16:29:36 +0800

    水水水水

---
+++ ttttest.py
@@ -0,0 +1,15 @@
+'''
+--------------------------------------------------------
+@File    :   ttttest.py    
+@Contact :   1183862787@qq.com
+@License :   (C)Copyright 2017-2018, CS, WHU
+
+@Modify Time : 2019/4/29 13:00     
+@Author      : Liu Wang    
+@Version     : 1.0   
+@Desciption  : None
+--------------------------------------------------------  
+''' 
+a = True
+b = False
+print(a*10, b*10)
